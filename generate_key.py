3c785477b71c0c9279b0a8c132863441ce76eb2b
ItzGalaxy15 <152269683+ItzGalaxy15@users.noreply.github.com>
2025-06-11 23:28:56 +0200

    Update generate_key.py

--- generate_key.py
+++ generate_key.py
@@ -7,3 +7,9 @@ with open("secret.key", "wb") as key_file:
     key_file.write(key)
 
 print("Key generated and saved to secret.key")
+
+def load_key():
+    with open("secret.key", "rb") as key_file:
+        return key_file.read()
+
+fernet = Fernet(load_key())
