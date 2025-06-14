2c0d9ac429be889b3c3078c1b110b2c79042ad95
Amer Alhasoun <ameralhasoun@gmail.com>
2025-06-14 23:34:09 +0200

    update menu_config.py #21

--- dashboard/menu_config.py
+++ dashboard/menu_config.py
@@ -2,7 +2,7 @@ from controllers.traveller_controller import TravellerController
 from controllers.usercontroller import UserController
 from controllers.session import UserSession
 from dashboard.actions import change_password_flow, add_user_flow
-from dashboard.actions import add_traveller_flow, update_traveller_flow, delete_traveller_flow, search_traveller_flow
+from dashboard.actions import add_traveller_flow, update_traveller_flow, delete_traveller_flow, search_traveller_flow, manage_traveller_flow
 
 def get_menu(session):
     return [
@@ -12,21 +12,8 @@ def get_menu(session):
         ("Add user", ("system_admin", "super"),
          UserController.add_user_controller, lambda: add_user_flow(session)),
 
-        ("Add traveller",   ("system_admin", "super"),
-         TravellerController.add_traveller_controller,
-         lambda: add_traveller_flow(session)),
-
-        ("Update traveller", ("system_admin", "super"),
-         TravellerController.update_traveller_controller,
-         lambda: update_traveller_flow(session)),
-
-        ("Delete traveller", ("system_admin", "super"),
-         TravellerController.delete_traveller_controller,
-         lambda: delete_traveller_flow(session)),
-
-        ("Search travellers", ("system_admin", "super"),
-         TravellerController.search_travellers_controller,
-         lambda: search_traveller_flow(session)),
+        ("Manage travellers", ("system_admin", "super"),
+        None, lambda: manage_traveller_flow(session)),
 
         ("Logout", None, None, session.logout)
     ]
