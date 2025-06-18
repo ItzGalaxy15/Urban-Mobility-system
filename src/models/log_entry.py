6dd5e392ed8b37221c71a1b6987ce560f840543e
Amer Alhasoun <ameralhasoun@gmail.com>
2025-06-18 18:48:30 +0200

    update the object LogEntry in log_entry.py #42

--- src/models/log_entry.py
+++ src/models/log_entry.py
@@ -1,14 +1,10 @@
 # models/log_entry.py
 import re
 from datetime import datetime
-from models.user import USERNAME_RE
 
 class LogEntry:
     def __init__(self, user_id: str, username: str, description: str,
                  *, additional: str = "", suspicious: bool = False, log_id: int = None):
-        
-        if not USERNAME_RE.fullmatch(username):
-            raise ValueError("username does not meet format/length rules")
 
         self.user_id = user_id
         now = datetime.now()
@@ -32,14 +28,3 @@ class LogEntry:
             f"description={self.description}, additional={self.additional}, "
             f"suspicious={self.suspicious})"
         )
-        
-
-# This mehtod could be used when you want to add a logEntry to the log_file.txt
-# def append_log(entry: LogEntry, path: str = "log_file.txt") -> None:
-#     # 1. serialize
-#     line = entry.as_csv()
-#     # 2. encrypt full line
-#     token = encrypt(line)          # => bytes
-#     # 3. write token as base64 string (one line)
-#     with open(path, "ab") as fh:
-#         fh.write(token + b"\n")
